1bd75d21a8157deb700e0df94953b4f315fbfd28
Eder <chlrobert93@gmail.com>
2021-08-19 17:04:37 -0500

    Agregar más componentes

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,15 +1,15 @@
 function Todo(props) {
-
- function onButtonClick (){
-       console.log('Onclick');
-       console.log(props.text);
- };
+  function onButtonClick() {
+    console.log(props.text);
+  }
 
   return (
     <div className="card">
       <h2>{props.text}</h2>
       <div className="actions">
-        <button onClick={() => { console.log('Hola')}} className="btn">Delete</button>
+        <button onClick={onButtonClick} className="btn">
+          Delete
+        </button>
       </div>
     </div>
   );
