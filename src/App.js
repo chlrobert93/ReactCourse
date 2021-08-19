69aaf5751c80f81fef27af50133860e06bc7e887
Eder <chlrobert93@gmail.com>
2021-08-19 13:56:47 -0500

    Estilo de componentes con clases CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import './App.css';
-
 function App() {
-  return <div>Hellow!</div>  
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <div className='card'>
+        <h2>Title</h2>
+        <div className='actions'>
+          <button className="btn">Delete</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
