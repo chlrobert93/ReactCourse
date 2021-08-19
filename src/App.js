ecae29f86f934ad6b50ad1034ae53d1d40f42197
Eder <chlrobert93@gmail.com>
2021-08-19 15:13:54 -0500

    Pasar datos con props y contenido dinámico

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import Todo from "./components/Todo";
+
 function App() {
   return (
     <dvi>
-      <Todo />
-      <Todo />
-      <Todo />
-      <Todo />
-      <Todo />
-      <Todo />
-      <Todo />
+      <Todo text='Learn React' />
+      <Todo text='Master React' />
+      <Todo text='Explore the full React course' />
     </dvi>
   );
 }
